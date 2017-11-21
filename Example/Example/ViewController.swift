9e4d92f96b1a41c00681daaf4305030edf75410e
farhad faramarzi <farhad.public@gmail.com>
2017-11-21 14:00:33 +0330

    Example

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Farhad on 11/21/17.
+//  Copyright © 2017 Farhad. All rights reserved.
+//
+// Sample Code
+
+import UIKit
+import PersianDatePicker
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var persianDatePickerView: PersianDatePickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        persianDatePickerView.onChange = {(year, month, day) in
+            print("\(year)/\(month)/\(day)")
+        }
+    }
+}
+
